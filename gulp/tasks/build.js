52cbfa6bf25feb305748ccdd6c30ddaddc171f07
Asaf Ben-Hamo <asaf.bh25@gmail.com>
2018-04-15 20:13:16 +0300

    coplite all files

---
+++ gulp/tasks/build.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    browser_sync = require('browser-sync').create();
+
+
+gulp.task('previewDist',function(){
+	 browser_sync.init({
+     notify:false,
+     server:{
+      baseDir:'docs'
+     }
+  });
+});
+
+
+gulp.task('deleteDistFolder',['icons'],function(){
+	return del('./docs');
+});    
+
+
+
+gulp.task('optimizeImages',['deleteDistFolder'],function () {
+	return gulp.src( [ './app/assets/images/**/*' , '!./app/assets/images/icons' , '!./app/assets/images/icons/**/*' ] )
+	    .pipe( imagemin( {
+	    	progressive:true,
+	    	interlaced:true,
+	    	multipass:true
+	    } ) )
+
+	    .pipe( gulp.dest('./docs/assets/images') )  ;
+});
+	    
+
+gulp.task('useminTrigger',['deleteDistFolder'],function(){
+    gulp.start('useminfy');
+});
+
+
+gulp.task('useminfy',['styles','scripts'],function() {
+	return gulp.src('./app/index.html') 
+	   .pipe( usemin( {
+	   	    css:[ function(){ return rev(); } , function(){ return cssnano(); } ] ,
+	   	    js:[ function(){ return rev(); } , function(){ return uglify(); } ]
+	   } ) )
+	   .pipe( gulp.dest('./docs') );
+});
+
+
+
+ gulp.task('build',['deleteDistFolder','optimizeImages','useminTrigger']);
+
+ 
+
